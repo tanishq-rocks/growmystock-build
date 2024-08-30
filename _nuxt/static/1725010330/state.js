52634d1784bdde389a7f963803402c496342a4d8
Tanishq Patni <tanishqlearn@gmail.com>
2024-08-30 15:02:15 +0530

    update build

---
+++ _nuxt/static/1725010330/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1725010330",layout:"default",error:a,state:{ui:{uiState:1725010329954,nextRoute:b,systemMsgs:[],menuSelected:a}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
