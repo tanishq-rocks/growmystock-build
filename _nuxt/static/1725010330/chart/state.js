f35721e31382aefb096af1d21f2df6e57dd9a1bc
Tanishq Patni <tanishqlearn@gmail.com>
2024-08-30 15:17:07 +0530

    update build

--- _nuxt/static/1725010330/chart/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1725010330",layout:"default",error:a,state:{ui:{uiState:1725010329888,nextRoute:b,systemMsgs:[],menuSelected:a}},serverRendered:true,routePath:"\u002Fchart",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
---
+++ _nuxt/static/1725011221/older-stock-picks/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1725011221",layout:"default",error:a,state:{ui:{uiState:1725011221591,nextRoute:b,systemMsgs:[],menuSelected:a}},serverRendered:true,routePath:"\u002Folder-stock-picks",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
